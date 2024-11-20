5955eebad8a6631d85dbd5b299dccbc00adbf9cc
Leonardo-snts <leonardosantos.ms77@gmail.com>
2024-11-20 02:25:40 -0400

    deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { Task } from "../pages/TaskList";
 
 const api = axios.create({
-  baseURL: "http://18.228.11.240:8080/api/",
+  baseURL: "http://18.228.11.240:8080/api/", 
+  // baseURL: "http://127.0.0.1:8000/api/",
 });
 
 export const fetchTasks = async () => {
